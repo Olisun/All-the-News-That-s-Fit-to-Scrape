e71ea28e7fc9ab2461da4a8b78f00f566b1c1d33
Oliver Sun <olisun@mac.com>
2019-10-25 22:08:05 -0700

    deploy

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -11,7 +11,7 @@ $(document).on("click", ".notes", function() {
       $("#notes").append("<h2>" + data.title + "</h2>");
       $("#notes").append("<input id='titleinput' name='title' >");
       $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");ß
+      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
       if (data.note) {
         $("#titleinput").val(data.note.title);
         $("#bodyinput").val(data.note.body);
